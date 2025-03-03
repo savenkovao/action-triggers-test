9a7406a5dfb552bc861ce54f1c3694c72f76aa81
artyom.savienkov <artyom.savienkov@glassboxdigital.com>
2025-03-03 15:31:05 +0200

    update link

--- stud/scripts/detector-config.js
+++ stud/scripts/detector-config.js
@@ -1,52 +1,34 @@
 if (typeof _cls_config === "undefined") {
   _cls_config = {};
 } /* DO NOT CHANGE */
-_cls_config.onReadyCallback = "test";
-_cls_config.ajaxRecordMetadata = "always";
-_cls_config.ajaxRecordResponseHeaders = "always";
-_cls_config.ajaxRecordResponseBody = "and(tld,statusgte(400))";
-_cls_config.clientAttributesEnabled = true;
-_cls_config.ajaxRecordStats = "always";
-_cls_config.valueMaskingMode = "blacklist";
-_cls_config.resourcesRecordEnabled = true;
-_cls_config.resourceRecordCssOnly = true;
-_cls_config.resourcesRecordChance = 0.9;
-_cls_config.webVitalsRecordEnabled = true;
-_cls_config.resourceTimingRecordEnabled = true;
-_cls_config.resourceTimingRecordEnabledByChance = 1;
-_cls_config.enableGoogleAnalyticsIntegration = true;
-_cls_config.googleDimensionIndex = 3;
-_cls_config.googleMeasurementId="UA-172635256-1";
-_cls_config.collectUsabillaVoc = true;
-_cls_config.collectVoc = true;
-_cls_config.sendTopURL = true;
-_cls_config.recordScrollReach = true;
-_cls_config.interceptABTesting = true;
-_cls_config.interceptThirdPartyAttributes = true;
-_cls_config.abTestingGlobalObject = "ttMeta";
-_cls_config.interceptAjax = true;
-_cls_config.enableSessionizingByCors=true;
-_cls_config.enableOptimizelyIntegration = true;
-_cls_config.supportRemoteClientConfig = false;
-_cls_config.vocObjectMap = {
-  "usabilla":{
-    "nps": {"name": "nps", "type": "int"},
-    "rating": {"name": "csat", "type": "int"},
-    "mood": {"name": "csat", "type": "int"},
-    "performance": {"name": "performance", "type": "int"},
-    "email": {"name": "email", "type": "string"},
-    "comment": {"name": "comment", "type": "string"},
-    "Comment": {"name": "comment", "type": "string"},
-    "action": {"name": "feedbackType", "type": "string"},
-    "id": {"name": "campaignId", "type": "string"},
-    "type": {"name": "campaignType", "type": "string"},
-    "custom rating": {"name": "customLabel", "type": "int"}
-  }
-};
-_cls_config.initDetectorOnInteractive = true;
+
+_cls_config = Object.assign({}, _cls_config, {
+  interceptAjax: true,
+  valueMaskingMode: "blacklist",
+  supportRemoteClientConfig: false,
+  clientAttributesEnabled: true,
+  collectVoc: true,
+  reportInitializeWorkerAsBlob: true,
+  reportUseWorker: true,
+  observeRightClick: true,
+  waitForSegmentRender: 10,
+  domFormAnalysisReporting: true,
+  domIncludeCSSSelector: true,
+  ajaxRecordStats: "always",
+  ajaxRecordResponseHeaders: "always",
+  ajaxRecordRequestHeaders: "always",
+  ajaxRecordResponseBody: "always",
+  ajaxRecordRequestBody: 'always',
+  ajaxRecordMetadata: 'always',
+  ajaxCaptureRequestCookie: true,
+  recordAjaxCallsAnyway: true,
+  iframesAutoInject: true,
+  ajaxTimeoutForResourceData: 0,
+})
+
 _cls_config.detectorPath = 'https://cdn.gbqofs.com/sv/a/';
-// _cls_config.reportURI = "https://report.dev-mt-eks.glassboxrnd.com/f3lx7s0z/reporting/6de4dfed-7a0a-7e37-6bed-2234c5e57113/cls_report"; // pre-release app
-_cls_config.reportURI = "https://feature-branch-report.glassboxrnd.com/feature-voc-publisher/0ijnbzfw/reporting/8757114c-07fd-3ca7-7f43-9a3340716709/cls_report"; // pre-release app
+_cls_config.reportURI = "https://report.dev-mt-eks.glassboxrnd.com/f3lx7s0z/reporting/6de4dfed-7a0a-7e37-6bed-2234c5e57113/cls_report"; // pre-release app
+// _cls_config.reportURI = "https://feature-branch-report.glassboxrnd.com/feature-voc-publisher/0ijnbzfw/reporting/8757114c-07fd-3ca7-7f43-9a3340716709/cls_report"; // pre-release app
 // _cls_config.reportURI = "https://feature-branch-report.glassboxrnd.com/feature-vos-188/3ygruafd/reporting/f3f0fa96-a08e-81d6-bb7f-1a04f07c1ebd/cls_report"; // pre-release app
 // _cls_config.reportURI = "https://feature-branch-report.glassboxrnd.com/feature-vos-qu/eiwfopcb/reporting/72ac168b-8b95-b22c-0039-a48e8b268ca8/cls_report"; // pre-release app
 // _cls_config.reportURI = "https://feature-branch-report.glassboxrnd.com/feature-vos-qu/eiwfopcb/reporting/11bc79ea-d571-b9ba-a8c0-b1e1f320b1a8/cls_report"; // strudprofkom
