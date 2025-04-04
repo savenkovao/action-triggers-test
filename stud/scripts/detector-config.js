c038a97e0d42b1740ad3eca9b2888ec5c90c9da3
artyom.savienkov <artyom.savienkov@glassboxdigital.com>
2025-04-04 15:38:36 +0300

    update link

--- stud/scripts/detector-config.js
+++ stud/scripts/detector-config.js
@@ -24,12 +24,9 @@ _cls_config = Object.assign({}, _cls_config, {
   recordAjaxCallsAnyway: true,
   iframesAutoInject: true,
   ajaxTimeoutForResourceData: 0,
-  reportURI: "https://feature-branch-report.glassboxrnd.com/feature-voc-publisher/0ijnbzfw/reporting/8757114c-07fd-3ca7-7f43-9a3340716709/cls_report"
+  reportURI: "https://report.dev-mt-eks.glassboxrnd.com/f3lx7s0z/reporting/d09b7249-e8b5-76c8-c8f7-c906a663701b/cls_report"
 })
 
 // _cls_config.detectorPath = 'https://cdn.gbqofs.com/sv/a/';
-// _cls_config.reportURI = "https://feature-branch-report.glassboxrnd.com/feature-voc-publisher/0ijnbzfw/reporting/8757114c-07fd-3ca7-7f43-9a3340716709/cls_report"; // pre-release app
-// _cls_config.reportURI = "https://feature-branch-report.glassboxrnd.com/feature-vos-188/3ygruafd/reporting/f3f0fa96-a08e-81d6-bb7f-1a04f07c1ebd/cls_report"; // pre-release app
-// _cls_config.reportURI = "https://feature-branch-report.glassboxrnd.com/feature-vos-qu/eiwfopcb/reporting/72ac168b-8b95-b22c-0039-a48e8b268ca8/cls_report"; // pre-release app
-// _cls_config.reportURI = "https://feature-branch-report.glassboxrnd.com/feature-vos-qu/eiwfopcb/reporting/11bc79ea-d571-b9ba-a8c0-b1e1f320b1a8/cls_report"; // strudprofkom
-// _cls_config.reportURI = "https://dev-7.0.0-report.glassboxrnd.com/f3lx7s0z/reporting/3009c3ac-51e9-21ab-7756-eef66586a0c6/cls_report"; // CHANGE
+// reportURI = "https://feature-branch-report.glassboxrnd.com/feature-voc-publisher/0ijnbzfw/reporting/8757114c-07fd-3ca7-7f43-9a3340716709/cls_report"; // pre-release app
+// reportURI: "https://report.dev-mt-eks.glassboxrnd.com/f3lx7s0z/reporting/d09b7249-e8b5-76c8-c8f7-c906a663701b/cls_report"
